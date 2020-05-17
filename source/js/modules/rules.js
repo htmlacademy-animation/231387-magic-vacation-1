ba1fbcfa94e30d85d0644ad11a0ac0bf77413d57
Bornyakova <darina.bornyakova@itglobal.com>
2020-05-17 22:21:51 +0300

    work on fixes; took back animation for header links; fix backticks in rules.js; px to rem in animations.scss

--- source/js/modules/rules.js
+++ source/js/modules/rules.js
@@ -1,9 +1,7 @@
 export default () => {
-    let rulesLastElement = document.querySelector('.rules__item:last-child p');
-    let rulesAction = document.querySelector('.rules__link');
-
-    console.log(rulesLastElement)
-    rulesLastElement.addEventListener(`animationend`, () => {
-        rulesAction.classList.add('active');
-    });
+  let rulesLastElement = document.querySelector(`.rules__item:last-child p`);
+  let rulesAction = document.querySelector(`.rules__link`);
+  rulesLastElement.addEventListener(`animationend`, () => {
+    rulesAction.classList.add(`active`);
+  });
 };
